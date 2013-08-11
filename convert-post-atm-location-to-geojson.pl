5aa2b49aa343b5e1499c165a1fbfe27e14081e28
Kang-min Liu <gugod@gugod.org>
2013-08-11 09:42:57 +0200

    build a geojson file out of ATM locations

---
+++ convert-post-atm-location-to-geojson.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+use v5.14;
+use autodie;
+use Parse::CSV;
+use List::MoreUtils "uniq";
+use JSON::PP 'encode_json';
+
+my $geo = { type => "FeatureCollection", features => [ ] };
+
+my $fh;
+open $fh, "<:utf8", "data/post_atm_location.csv";
+
+my $csv = Parse::CSV->new(
+    handle => $fh,
+    names  => ["city", "district", "serial", "description", "tel", "addr", "longitude", "latitude", "x1","x2","x3","x4","position"]
+);
+
+while (my $row = $csv->fetch) {
+    my $label = join ",", uniq grep { $_ } @{$row}{"position", "x1", "x2", "x3", "x4"};
+
+    push @{$geo->{features}}, {
+        type => "Feature",
+        geometry => {
+            type => "Point",
+            coordinates => [ $row->{longitude}, $row->{latitude} ]
+        },
+        properties => {
+            'marker-symbol' => $label,
+            name => $row->{description},
+            address => "$row->{city} $row->{district} $row->{addr}"
+        }
+    };
+    ;
+}
+if ($csv->errstr) {
+    say "ERROR: ".$csv->errstr;
+}
+
+open my $out, ">", "data/post_atm_location.geojson";
+print $out JSON::PP->new->utf8->pretty->encode($geo);
